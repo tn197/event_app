12ee57cbde79835c84618bab402e7fcdcba7c6af
Ubuntu <ubuntu@ip-172-31-28-238.us-east-2.compute.internal>
2020-06-29 10:01:56 +0000

    GuestController作成,マイグレーションファイルリセット更新

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
 　has_secure_token :uuid
+　
+　before_validation :set_uuid 
   
   has_many :guest, through: :events_guest
   has_many :events_guest
@@ -8,4 +10,10 @@ class Event < ApplicationRecord
     
 　validates :title, presence: true  
 　validates :content, presence: true
+
+  def set_uuid
+    aaaa
+    uuid = SecureRandom.uuid #has_secure_tokenが動かないため
+  end
+
 end
